ad8b0be405d700d0f3e0c3574a051b9bac0513ce
MarekNekdo <info@nasestrana.cz>
2025-04-13 17:35:35 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 3s infinite',
+        'pulse-fast': 'pulse 0.7s infinite',
+      },
+    },
+  },
+  plugins: [],
+}
